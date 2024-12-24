d831a40090a1ae9428b52b4cc3cc23ca40375642
Abdelrahman Ahmed <supersanko2002@gmail.com>
2024-12-24 22:11:43 +0200

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hobignejticnjslluwhj.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
